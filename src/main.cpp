d59ac3705818c9a16e2699229e6df58981496704
philipb78 <philip_black55@hotmail.com>
2025-04-10 12:28:55 +0100

    test engine flash

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,15 @@ void setup()
   _boxWebServer = new BoxWebServer(_mockData);
   _seaTalk = new SeaTalk();
   _testEngine = new TestEngine(_mockData, _seaTalk);
+
+  Serial.println("\n##################################");
+  Serial.println(F("ESP32 Information:"));
+  Serial.printf("Internal Total Heap %d, Internal Used Heap %d, Internal Free Heap %d\n", ESP.getHeapSize(), ESP.getHeapSize() - ESP.getFreeHeap(), ESP.getFreeHeap());
+  Serial.printf("Sketch Size %d, Free Sketch Space %d\n", ESP.getSketchSize(), ESP.getFreeSketchSpace());
+  Serial.printf("SPIRam Total heap %d, SPIRam Free Heap %d\n", ESP.getPsramSize(), ESP.getFreePsram());
+  Serial.printf("Chip Model %s, ChipRevision %d, Cpu Freq %d, SDK Version %s\n", ESP.getChipModel(), ESP.getChipRevision(), ESP.getCpuFreqMHz(), ESP.getSdkVersion());
+  Serial.printf("Flash Size %d, Flash Speed %d\n", ESP.getFlashChipSize(), ESP.getFlashChipSpeed());
+  Serial.println("##################################\n");
 }
 
 void loop()
